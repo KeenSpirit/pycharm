23f5a23f403486400f2be0b2abc2b817c0a145c7
KeenSpirit <danjp62@hotmail.com>
2022-03-13 14:09:43 +1000

    new function

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def my_function(a,b):
+    return a+b
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Friend')
     print("How are ya?")
+
+    print(my_function(2,4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
